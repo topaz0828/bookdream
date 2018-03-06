54db00de30ba517aef8146c750899c460d9d1530
jh <freejh21@gmail.com>
2018-03-06 17:59:38 +0900

    DB에서 json 형태로 단순하게 select하는 로직 추가

---
+++ reviews/reviewDream/src/main/java/hellobro/reviews/ReviewDreamApplication.java
@@ -0,0 +1,16 @@
+package hellobro.reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+//@EnableDiscoveryClient
+public class ReviewDreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewDreamApplication.class, args);
+    }
+
+}
