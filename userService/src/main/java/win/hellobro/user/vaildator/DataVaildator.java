0f31026ef50dc5e3bc1e7192f9af27a5cc163cf0
topaz0828 <topaz0828@gmail.com>
2018-03-26 09:53:38 +0900

    add userService (vaildator)

---
+++ userService/src/main/java/win/hellobro/user/vaildator/DataVaildator.java
@@ -0,0 +1,53 @@
+package win.hellobro.user.vaildator;
+
+import com.mysql.cj.core.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import win.hellobro.user.exception.UserServiceException;
+
+@Service
+public class DataVaildator {
+
+    @Value("${data-validation.len.id}")
+    private int LEN_ID;
+
+    @Value("${data-validation.len.email}")
+    private int LEN_EMAIL;
+
+    @Value("${data-validation.len.nickname}")
+    private int LEN_NICKNAME;
+
+    @Value("${data-validation.len.oauthsite}")
+    private int LEN_OAUTHSITE;
+
+    @Value("${data-validation.len.image}")
+    private int LEN_IMAGE;
+
+    private int checkMaxSize(String value) throws UserServiceException {
+        int maxSize = 0;
+        if ("id".equalsIgnoreCase(value)) maxSize = LEN_ID;
+        if ("email".equalsIgnoreCase(value)) maxSize = LEN_EMAIL;
+        if ("nickname".equalsIgnoreCase(value)) maxSize = LEN_NICKNAME;
+        if ("from".equalsIgnoreCase(value)) maxSize = LEN_OAUTHSITE;
+        if ("image".equalsIgnoreCase(value)) maxSize = LEN_IMAGE;
+        return maxSize;
+    }
+
+    public void isValidMandantory(String param, String value) throws UserServiceException {
+        if (StringUtils.isNullOrEmpty(value))
+            throw new UserServiceException(HttpStatus.BAD_REQUEST, String.format("%s [%s] is null or empty", param, value));
+
+        if(value.length() > checkMaxSize(param))
+            throw new UserServiceException(HttpStatus.BAD_REQUEST, String.format("$s [%s] data size exceed maxSize", param, value));
+    }
+
+    public void isValidOption(String param, String value) throws UserServiceException {
+        if(value.length() > checkMaxSize(param))
+            throw new UserServiceException(HttpStatus.BAD_REQUEST, String.format("%s [%s] data size exceed maxSize", param, value));
+    }
+
+
+
+
+}
